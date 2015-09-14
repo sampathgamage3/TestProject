f2ee8104f3bfa0c5d6a717bbc45c902591658ab1
sampath <sampath@eight25media.com>
2015-09-14 09:43:02 +0530

    added code diasabled core updates and themes/plugins Please enter the commit message for your changes. Lines starting

--- functions.php
+++ functions.php
@@ -138,4 +138,25 @@ function theme_featured_image_meta( $content ) {
         }
     }
 
+    
+    // remove core updates
+    
+    add_action('after_setup_theme','remove_core_updates');
+	function remove_core_updates()
+	{
+	 if(! current_user_can('update_core')){return;}
+	 add_action('init', create_function('$a',"remove_action( 'init', 'wp_version_check' );"),2);
+	 add_filter('pre_option_update_core','__return_null');
+	 add_filter('pre_site_transient_update_core','__return_null');
+	}
+	
+	remove_action('load-update-core.php','wp_update_plugins');
+	add_filter('pre_site_transient_update_plugins','__return_null');
+	
+	 
+	
+	add_filter('pre_site_transient_update_core','remove_core_updates');
+	add_filter('pre_site_transient_update_plugins','remove_core_updates');
+	add_filter('pre_site_transient_update_themes','remove_core_updates');
+	
 
